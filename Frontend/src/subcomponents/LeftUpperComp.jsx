ddff26ef9b6dc86ce736dea66b74a3e74ccfb71a
lazyScriptor <theekshana.jny@gmail.com>
2024-08-02 17:37:58 +0530

    1

--- Frontend/src/subcomponents/LeftUpperComp.jsx
+++ Frontend/src/subcomponents/LeftUpperComp.jsx
@@ -5,12 +5,31 @@ import {
   Input,
   InputLabel,
   TextField,
+  Button,
   Box,
   FormLabel,
+  Grid,
+  Paper,
 } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+import Accordion from "@mui/material/Accordion";
+import AccordionActions from "@mui/material/AccordionActions";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+
 import React from "react";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
 function LeftUpperComp() {
   return (
@@ -50,6 +69,47 @@ function LeftUpperComp() {
           <LocalizationProvider dateAdapter={AdapterDayjs}>
             <DatePicker size="small" />
           </LocalizationProvider>
+
+          {/* Accordian starting from here */}
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1-content"
+              id="panel1-header"
+            >
+              President
+            </AccordionSummary>
+            <AccordionDetails
+              component={Box}
+              sx={{
+                display: "flex",
+                justifyContent: "start",
+                flexDirection: "column",
+                gap: 2,
+              }}
+            >
+              <FormLabel align="left">Name of the Trade Union</FormLabel>
+              <TextField
+                id="outlined-size-small"
+                placeholder="Enter name of the Trade Union"
+                size="small"
+              />
+              <Grid container spacing={2}>
+                <Grid item xs={6}>
+                  <Item></Item>
+                </Grid>
+                <Grid item xs={6}>
+                  <Item></Item>
+                </Grid>
+              </Grid>
+              <FormLabel align="left">Name of the Trade Union</FormLabel>
+              <TextField
+                id="outlined-size-small"
+                placeholder="Enter name of the Trade Union"
+                size="small"
+              />
+            </AccordionDetails>
+          </Accordion>
         </FormControl>
       </Box>
     </Box>
